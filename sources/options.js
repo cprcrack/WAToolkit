81e5a620c6d2314c5de5f6511c15bc0a88c7834e
Cristian Perez <cpr@cpr.name>
2015-02-01 21:53:42 +0100

    Added new options page system (Chrome 40+)

---
+++ sources/options.js
@@ -0,0 +1,26 @@
+﻿/*
+WAToolkit
+Author: Cristian Perez <http://www.cpr.name>
+License: GNU GPLv3
+*/
+
+
+var whatsAppUrl = "https://web.whatsapp.com/";
+
+var counter = 10;
+var counterElem = document.getElementById("counter");
+
+setInterval(function()
+{
+    counter--;
+    counterElem.innerHTML = counter;
+
+    if (counter < 1)
+    {
+        setTimeout(function ()
+        { 
+            window.location.href = whatsAppUrl;
+            window.close();
+        }, 100);
+    }
+}, 1000); 
