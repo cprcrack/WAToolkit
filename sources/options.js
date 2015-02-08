151d434682cfe540844864b79396900bfcb96889
Cristian Perez <cpr@cpr.name>
2015-02-08 03:04:51 +0100

    Better native options dialog redirection for both Extensions page and options page via tray icon click

--- sources/options.js
+++ sources/options.js
@@ -7,20 +7,4 @@ License: GNU GPLv3
 
 var whatsAppUrl = "https://web.whatsapp.com/";
 
-var counter = 10;
-var counterElem = document.getElementById("counter");
-
-setInterval(function()
-{
-    counter--;
-    counterElem.innerHTML = counter;
-
-    if (counter < 1)
-    {
-        setTimeout(function ()
-        { 
-            window.location.href = whatsAppUrl;
-            window.close();
-        }, 100);
-    }
-}, 1000); 
+window.location.href = whatsAppUrl;
